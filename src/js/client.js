d59fe5357340833b87477b48df1acaa1d7b8e5e2
aratius <arata1129matsu@icloud.com>
2020-12-31 13:17:35 +0900

    reduxの下準備

--- src/js/client.js
+++ src/js/client.js
@@ -1,18 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
-import Message from './modules/message'
+import App from './modules/app'
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <Message>
-        <p>こんにちは</p>
-        <div>ディブです</div>
-      </Message>
-    );
+//ステートの値
+let state_value = {
+  counter: 0,
+  message: 'hello'
+}
+
+//レデューサー
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+        message: 'INCREMENT'
+      };
+      case 'DECREMENT':
+        return {
+          counter: state.counter - 1,
+          message: 'DECREMENT'
+        }
+      default:
+        return state;
   }
 }
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+
+//ストア作成
+let store = createStore(counter);
+
+//表示をレンダリング
+ReactDOM.render(
+  //Provider内部のコンポーネントにstoreの情報が渡される
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class Message extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     console.log(this.props.children);
