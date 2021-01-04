fb6c2bb239959577b8ac95266c1a924afe05e533
aratius <arata1129matsu@icloud.com>
2021-01-04 22:59:20 +0900

    redux-promise-middlewareを使うと非同期実行状態のハンドリング記述が簡潔になる

--- src/js/redux.js
+++ src/js/redux.js
@@ -4,7 +4,7 @@ https://qiita.com/TsutomuNakamura/items/2ded5112ca5ded70e573
 
  import { applyMiddleware, createStore } from 'redux'
  import { createLogger } from 'redux-logger'
- import thunk from 'redux-thunk'
+ import promise from 'redux-promise-middleware'
  import axios from 'axios'
 
  //reducer
@@ -17,30 +17,25 @@ https://qiita.com/TsutomuNakamura/items/2ded5112ca5ded70e573
 
  const reducer = (state = initalState, action)=> {
    switch(action.type) {
-    case "FETCH_USERS_START":  //処理中
+    case "FETCH_USERS_PENDING":  //処理中
       return {...state, fetching: true};
-    case "RECIEVE_USERS":  //処理完了
+    case "FETCH_USERS_FULFILLED":  //処理完了
       return {...state, fetching: false, fetched: true, users: action.payload};
-    case "FETCH_USERS_ERROR":
+    case "FETCH_USERS_REJECTED":  //エラー
       return {...state, fetching: false, error: action.payload};
    }
    return state;
  }
 
 //  redux-thunkはactionの代わりに非同期処理を渡して、それが終わった後に通常の同期処理アクションをdispatchできるようにするためのやつ
- const middleware = applyMiddleware(thunk, createLogger());
- const store = createStore(reducer, middleware);
+const middleware = applyMiddleware(promise, createLogger());
+const store = createStore(reducer, middleware);
 
- store.dispatch((dispatch) => {
-   dispatch({type: "FETCH_USERS_START"})
+store.dispatch({
+  type: "FETCH_USERS",
+  payload: axios.get("http://localhost:18080")
+})
 
-   //非同期処理ここから
-   axios.get("http://localhost:18080").then((response)=> {
-     dispatch({type: "RECIEVE_USERS", payload: response.data})
-   }).catch((err)=> {
-     dispatch({type: "FETCH_USERS_ERROR", payload: err})
-   })
- })
 
  //ダミーサーバ
 /*  $ node << EOF
