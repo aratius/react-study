762c794912be654a0cac4acfa3707165f418d584
aratius <arata1129matsu@icloud.com>
2021-01-04 13:25:22 +0900

    memoフォルダを作ってそこにまとめた

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path    = require('path');
 
 module.exports = {
   context: path.join(__dirname, "src"),
-  entry: "./js/client.js",
+  entry: "./js/memo.js",
   module: {
     rules: [{
       test: /\.jsx?$/,
